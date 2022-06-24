4bdf077abae02324a67a61341ba04bd381648531
Isaac Floriano <55949467+isaacfloriano@users.noreply.github.com>
2022-06-24 15:55:37 -0300

    Create ip.php

---
+++ ip.php
@@ -0,0 +1,23 @@
+<?php
+//função para pegar IP
+function get_client_ip() {
+    $ipaddress = '';
+    if (isset($_SERVER['HTTP_CLIENT_IP']))
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    else if(isset($_SERVER['REMOTE_ADDR']))
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    else
+        $ipaddress = 'DESCONHECIDO';
+    return $ipaddress;
+}
+//observação se for ultilizar em localhost use o ip da maquina
+//ex: 192.168.1.13/web/site/ip
+$getIP = get_client_ip();
